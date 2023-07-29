3f1216862eb9a1b271243ea3e3fe1f4b430f7e86
BNJIO <oelbanouji@gmail.com>
2023-07-28 18:10:01 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+    message db "Hello, Holberton", 10, 0
+section .text
+    global main
+    extern printf
+main:
+    push rbp
+    mov rbp, rsp
+    mov rdi, message
+    mov al, 0
+    call printf
+    mov rsp, rbp
+    pop rbp
+    ret
